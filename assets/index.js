3effa204ee197cd7bc809fe660ce726d9492b6b3
Andrew Read <aj.read13@gmail.com>
2022-01-04 14:42:53 -0500

    added some questions

---
+++ assets/index.js
@@ -0,0 +1,43 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    title: "What is the name of your project?",},
+    {
+    gitHubName: "What is your Git Hub user name?",},
+    {
+    projectDescription: "Please Desribe your project.",},
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
