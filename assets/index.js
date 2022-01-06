fb9fd3b82adb8f31a7d7b4336db90840117b8ffe
Andrew Read <aj.read13@gmail.com>
2022-01-05 20:41:30 -0500

    almost there!

--- assets/index.js
+++ assets/index.js
@@ -5,73 +5,67 @@ const fs = require('fs');
 const generateMarkdown = require('./generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your name?',
-      name: 'personName',
-    },
-    {
-      type: "input",
-      message: "What is the name of your project?",
-      name: "projectName",
-    },
-    {
-      type: 'list',
-      message: 'Which license would you like to use?',
-      name: "licenses",
-      choices: ["MIT", "ISC", "Artist License 2.0", "Apache License 2.0"],
-    },
-    {
-      type: 'input',
-      message: 'What is your GitHub username?',
-      name: 'gitUserName',
-    },
-    {
-      type: 'input',
-      message: 'Please describe your project.',
-      name: 'projectDescript',
-    },
-    {
-      type: 'input',
-      message: 'Please provide installation instruction.',
-      name: 'installGuide',
-    },
-    {
-      type: 'input',
-      message: 'Please provide how someone may contribute.',
-      name: 'contributions',
-    },
-    {
-      type: 'input',
-      message: 'How is the application used?',
-      name: 'usage',
-    },
-    {
-      type: 'input',
-      message: 'How was your application tested?',
-      name: 'tested',
-    },
-    {
-      type: 'input',
-      message: 'What is your email address?',
-      name: 'questions',
-    },
-  ])
-  .then((response) => {
-    console.log(response);
-    const fileName = `README.md`;
 
-    fs.appendFile(fileName, JSON.stringify(response),
-      (err) => err ? console.log(error) : console.log(response))
-  });
-
-
-
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your name?',
+    name: 'personName',
+  },
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "projectName",
+  },
+  {
+    type: 'list',
+    message: 'Which license would you like to use?',
+    name: "license",
+    choices: ["MIT", "ISC", "Apache License 2.0"],
+  },
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'gitUserName',
+  },
+  {
+    type: 'input',
+    message: 'Please describe your project.',
+    name: 'projectDescript',
+  },
+  {
+    type: 'input',
+    message: 'Please provide installation instruction.',
+    name: 'installGuide',
+  },
+  {
+    type: 'input',
+    message: 'Please provide how someone may contribute.',
+    name: 'contributions',
+  },
+  {
+    type: 'input',
+    message: 'How is the application used?',
+    name: 'usage',
+  },
+  {
+    type: 'input',
+    message: 'How was your application tested?',
+    name: 'tested',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+]
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+  
+    fs.writeFile(fileName, data,
+      (err) => err ? console.log(err) : console.log(data));
+ }
 
 // TODO: Create a function to initialize app
 function init() {
@@ -80,7 +74,8 @@ function init() {
     .then((response) => {
       console.log(response);
       const markdown = generateMarkdown(response);
-      const fileName = `${}`
+      const fileName = `README.md`;
+      writeToFile(fileName, markdown)
     })
  }
 
